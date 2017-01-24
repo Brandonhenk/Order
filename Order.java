d0fc656617406ed5b9fd99620b84d984b4d05202
Brandonhenk <bhenk8655@volmail.jalc.edu>
2017-01-24 06:52:04 -0600

    Update Order.java

--- Order.java
+++ Order.java
@@ -1,7 +1,14 @@
+import java.util.Scanner;
+import java.io.*;
 public class Order{
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
+
     for (int x = 0; x < args.length; x++){
-      
+      File file = new File(args[x]);
+      Scanner inputFile = new Scanner(file);
+
+      String line = inputFile.nextLine();
+      System.out.println(line);
     }
   }
 }
